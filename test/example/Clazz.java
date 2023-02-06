313471c893fd589aba42caca3279d860d98156f5
Alex <Korshunovych>
2023-02-06 16:54:24 +0200

    clazz change br2

--- test/example/Clazz.java
+++ test/example/Clazz.java
@@ -2,11 +2,13 @@ package test.example;
 
 public class Clazz {
 
+    //2nd commit for master
+
+    //3rd commit for master
+
     //1st commit from br3
 
     //2nd commit br3
 
-    //2nd commit for master
-
-    //3rd commit for master
+    // clazz change in br2
 }
